a5073a0e58c9351e0298c955d549589b4435077b
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-03-03 19:42:07 -0800

    endording and rout for urls_new is added

--- express_server.js
+++ express_server.js
@@ -10,6 +10,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,6 +19,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
