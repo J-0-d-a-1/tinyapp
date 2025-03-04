35f99c64315ca6263ae1e85837b17f30d08408d5
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-03-03 19:53:13 -0800

    created rout to match POST from form

--- express_server.js
+++ express_server.js
@@ -10,21 +10,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+// getting ready for the POST
 app.use(express.urlencoded({ extended: true }));
 
+// rout for /
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// rout for /urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// rout for /urls/new
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// rout for /urls/:id
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -39,6 +44,12 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// rout that will match POST request from form
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
